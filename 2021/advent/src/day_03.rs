5e2d19bb39cc62a241c20551728376df1a4ff6cc
Tyler Reid <jamestylerreid@gmail.com>
2021-12-03 10:53:46 -0500

    refactor part 2 to dedupe filter

--- 2021/advent/src/day_03.rs
+++ 2021/advent/src/day_03.rs
@@ -24,65 +24,42 @@ pub fn f() {
     println!("{:?}", gamma * epsilon);
 }
 
+const BITLEN: usize = 12;
 pub fn part2() {
-    const Bitlen: usize = 12;
     let input = input::read_parse(3, |x| i32::from_str_radix(x, 2).unwrap());
-    let mut bit_counts = [0; Bitlen];
-    let mut mask = 1 << (Bitlen - 1);
-    let mut oxygen_list = input.clone();
-    let mut oxygen = 0;
-
-    for i in (0..Bitlen).rev() {
-        for x in &oxygen_list {
-            bit_counts[i] += if x & mask == 0 { 0 } else { 1 };
-        }
-
-        let num_set = bit_counts[i];
-        let num_not_set = oxygen_list.len() - num_set;
-        oxygen_list.retain(|x| {
-            if num_set >= num_not_set {
-                x & mask != 0
-            } else {
-                x & mask == 0
-            }
-        });
-
-        if oxygen_list.len() == 1 {
-            oxygen = oxygen_list[0];
-            println!("found oxygen {}", oxygen);
-            break;
-        }
-
-        mask = mask >> 1;
-    }
-
-    mask = 1 << (Bitlen - 1);
-    bit_counts = [0; Bitlen];
-    let mut c02_list = input.clone();
-    let mut c02 = 0;
-    for i in (0..Bitlen).rev() {
-        for x in &c02_list {
-            bit_counts[i] += if x & mask == 0 { 0 } else { 1 };
-        }
-
-        let num_set = bit_counts[i];
-        let num_not_set = c02_list.len() - num_set;
-        c02_list.retain(|x| {
-            if num_set >= num_not_set {
-                x & mask == 0
-            } else {
-                x & mask != 0
-            }
-        });
-
-        if c02_list.len() == 1 {
-            c02 = c02_list[0];
-            println!("found c02 {}", c02);
-            break;
-        }
-
-        mask = mask >> 1;
-    }
+    let oxygen = part2_filter(&input, true);
+    let c02 = part2_filter(&input, false);
 
     println!("{}", oxygen * c02);
+}
+
+fn part2_filter(input: &Vec<i32>, is_oxygen: bool) -> i32 {
+    let mut bit_counts = [0; BITLEN];
+    let mut mask = 1 << (BITLEN - 1);
+
+    let mut list = input.clone();
+
+    for i in (0..BITLEN).rev() {
+        for x in &list {
+            bit_counts[i] += if x & mask == 0 { 0 } else { 1 };
+        }
+
+        let num_set = bit_counts[i];
+        let num_not_set = list.len() - num_set;
+        list.retain(|x| {
+            if num_set >= num_not_set {
+                (x & mask != 0) == is_oxygen
+            } else {
+                (x & mask == 0) == is_oxygen
+            }
+        });
+
+        if list.len() == 1 {
+            return list[0]
+        }
+
+        mask = mask >> 1;
+    }
+
+    0
 }
