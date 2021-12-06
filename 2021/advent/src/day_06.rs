f9b225afc7eb14cc4d311a1c8c57e77d549376e9
Tyler Reid <jamestylerreid@gmail.com>
2021-12-06 13:39:22 -0500

    simplify day 6

--- 2021/advent/src/day_06.rs
+++ 2021/advent/src/day_06.rs
@@ -1,35 +1,18 @@
 pub fn f() {
-    let input: Vec<u8> = std::fs::read_to_string("input/6")
+    let input: Vec<usize> = std::fs::read_to_string("input/6")
         .unwrap()
         .split(",")
-        .map(|x| x.parse::<u8>().unwrap())
+        .map(|x| x.parse::<usize>().unwrap())
         .collect();
 
-    let mut buckets: [usize; 9] = [
-        0,
-        input.iter().filter(|x| **x == 1).count(),
-        input.iter().filter(|x| **x == 2).count(),
-        input.iter().filter(|x| **x == 3).count(),
-        input.iter().filter(|x| **x == 4).count(),
-        input.iter().filter(|x| **x == 5).count(),
-        input.iter().filter(|x| **x == 6).count(),
-        0,
-        0,
-    ];
+    let mut buckets = [0; 9];
+    for f in input {
+        buckets[f] += 1;
+    }
 
     for _ in 0..256 {
-        tick(&mut buckets);
+        buckets.rotate_left(1);
+        buckets[6] += buckets[8];
     }
     println!("{}", buckets.iter().sum::<usize>())
-}
-
-fn tick(buckets: &mut [usize; 9]) {
-    let ready_to_spawn = buckets[0];
-
-    for i in 0..8 {
-        buckets[i] = buckets[i+1];
-    }
-
-    buckets[6] += ready_to_spawn;
-    buckets[8] = ready_to_spawn;
 }
