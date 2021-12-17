79487c71846c63a8baf98f8528adfa62060d6e60
Tyler Reid <jamestylerreid@gmail.com>
2021-12-17 09:29:55 -0500

    day 17 part 2

--- 2021/advent/src/day_17.rs
+++ 2021/advent/src/day_17.rs
@@ -5,6 +5,8 @@ pub fn f() {
     let target = parse_target(&std::fs::read_to_string("input/17").unwrap());
 
     println!("{:?}", target);
+    println!("{:?}", fire(&target, (6, 4)));
+
     fire_ze_missles(&target);
 }
 
@@ -15,12 +17,12 @@ fn parse_target(s: &str) -> (Range<i32>, Range<i32>) {
     let captures = regex.captures(s).unwrap();
     println!("{:?}", captures);
 
-    (
-        captures.get(1).unwrap().as_str().parse().unwrap()
-            ..captures.get(2).unwrap().as_str().parse().unwrap(),
-        captures.get(3).unwrap().as_str().parse().unwrap()
-            ..captures.get(4).unwrap().as_str().parse().unwrap(),
-    )
+    let x_start = captures.get(1).unwrap().as_str().parse().unwrap();
+    let x_end = captures.get(2).unwrap().as_str().parse::<i32>().unwrap() + 1;
+    let y_start = captures.get(3).unwrap().as_str().parse().unwrap();
+    let y_end = captures.get(4).unwrap().as_str().parse::<i32>().unwrap() + 1;
+
+    (x_start..x_end, y_start..y_end)
 }
 
 fn step((mut dx, mut dy): (i32, i32), (x, y): (i32, i32)) -> ((i32, i32), (i32, i32)) {
@@ -38,31 +40,39 @@ fn step((mut dx, mut dy): (i32, i32), (x, y): (i32, i32)) -> ((i32, i32), (i32, 
 
 fn fire_ze_missles(target: &(Range<i32>, Range<i32>)) {
     let mut max_y = 0;
-    for x in 0..100 {
-        for y in 0..10000 {
+    let mut hit_count = 0;
+    for x in -1000..1000 {
+        for y in -1000..1000 {
             if let Some(max) = fire(&target, (x, y)) {
+                hit_count += 1;
                 if max_y < max {
                     max_y = max;
                 }
             }
         }
     }
-    println!("{}", max_y);
+    println!("max y {}, hit count {}", max_y, hit_count);
 }
 
 fn fire(target: &(Range<i32>, Range<i32>), (mut dx, mut dy): (i32, i32)) -> Option<i32> {
     let mut position = (0, 0);
     let mut max_y = 0;
+    let og_velocity = (dx, dy);
     for _ in 0..1000 {
         let result = step((dx, dy), position);
         position = result.1;
-        dx = result.0.0;
-        dy = result.0.1;
+        dx = result.0 .0;
+        dy = result.0 .1;
+
+        if target.0.end < position.0 || target.1.start > position.1 {
+            return None
+        }
+
         if position.1 > max_y {
             max_y = position.1;
         }
         if target.0.contains(&position.0) && target.1.contains(&position.1) {
-            println!("found hit with {:?} and max {}", position, max_y);
+            println!("found hit in {:?} with velocity {:?} and max {}", position, og_velocity, max_y);
             return Some(max_y);
         }
     }
